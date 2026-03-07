102bfeaa14d03ff9b33fba8c860d869ce98286d3
parkminah123 <hello.git.kna9919@gmail.com>
2026-03-07 17:03:55 +0900

    edit script

--- src/main.js
+++ src/main.js
@@ -87,7 +87,7 @@ mm.add('(max-width: 640px)', () => {
   const imageZoom = gsap.timeline({
     scrollTrigger: {
       trigger: '#trigger1',
-      start: 'top bottom',
+      start: 'top bottom+200',
       end: 'bottom top',
       scrub: 0.5,
       invalidateOnRefresh: true
